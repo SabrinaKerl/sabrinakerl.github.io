16811eeae8a428c3269c6b2125d072f691c61689
Sabrina K <Sabrina K@141.28.229.28>
2018-03-28 22:36:54 +0200

    Userpage2

--- Aufgabe 0/Aufgabe0.js
+++
@@ -1,6 +0,0 @@
-function box(){
-    var input = prompt ("Hi, verrätst Du mir Deinen Namen?", "");
-    if (input != null){
-        document.getElementById ("Hi").innerHTML = "Danke Dir " + input;
-        }
-    }
